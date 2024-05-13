bb8f9cd50f2c8d45a629498de27ea2382fcae9ef
Adnan Sabbir <adns@odoo.com>
2024-05-13 15:48:44 +0400

    [IMP] so_with_versions: store version number in db and generate new name from that number

--- so_with_versions/models/sale_order.py
+++ so_with_versions/models/sale_order.py
@@ -3,25 +3,19 @@
 from odoo import models, fields, api
 
 
-def _extract_name_and_version(name):
-    if '-' in name:
-        name, version = name.rsplit('-', 1)
-        return name, int(version)
-    return name, 0
-
-
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    version_count = fields.Integer(default=0)
+
     def action_create_version(self):
         for record in self:
-            name, version = _extract_name_and_version(record.name)
             pdf, _ = self.env['ir.actions.report'].sudo()._render_qweb_pdf('sale.action_report_saleorder', [record.id])
 
             record.message_post(
-                attachments=[
-                    (f'{record.name}.pdf', pdf),
-                ]
+                attachments=[(f'{record.name}.pdf', pdf)]
             )
 
-            record.name = f'{name}-{version + 1}'
+            name = record.name.split('-')[0]
+            record.name = f'{name}-{record.version_count}'
+            record.version_count = record.version_count + 1
